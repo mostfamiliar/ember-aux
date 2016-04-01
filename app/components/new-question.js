44e3157d4d0204947845dff168cbad5d67463f45
torrence stratton <tlstratton@gmail.com>
2016-04-01 08:40:20 -0700

    add form for new question

---
+++ app/components/new-question.js
@@ -0,0 +1,13 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  addNewQuestion: false,
+  actions: {
+      questionFormShow() {
+        this.set('addNewQuestion', true);
+      },
+    saveQuestion(){
+      //logic here
+    }
+  }
+});
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,22 +1,22 @@
 import Ember from 'ember';
 
-
-var test = [{
-    username: "DLynch",
-    title: "How do I implement parallel compression?",
-    notes: "Using ProTools"
-    }, {
-    username: "THaynes",
-    title: "What compressor should I buy?",
-    notes: "None"
-    }, {
-      username: "WWenders",
-      title: "What gear did Mac Demarco use on Salad Days?",
-      notes: "Specifically, mics??"
-    }];
+//
+// var test = [{
+//     username: "DLynch",
+//     title: "How do I implement parallel compression?",
+//     notes: "Using ProTools"
+//     }, {
+//     username: "THaynes",
+//     title: "What compressor should I buy?",
+//     notes: "None"
+//     }, {
+//       username: "WWenders",
+//       title: "What gear did Mac Demarco use on Salad Days?",
+//       notes: "Specifically, mics??"
+//     }];
 
 export default Ember.Route.extend({
   model(){
-    return test;
+    return this.store.findAll('question');
   },
 });
