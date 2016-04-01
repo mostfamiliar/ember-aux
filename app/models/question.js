2e45068f2cf155bd74320e6248996252a4363e88
torrence stratton <tlstratton@gmail.com>
2016-04-01 10:59:19 -0700

    fix issue with answer model

--- app/models/question.js
+++ app/models/question.js
@@ -3,6 +3,6 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   username: DS.attr(),
   title: DS.attr(),
-  notes: DS.attr()
+  notes: DS.attr(),
   answers: DS.hasMany('answer', { async: true})
 });
